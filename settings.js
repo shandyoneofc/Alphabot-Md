5b108deced5d0f1d5baa06d32f4f575f3253d8b1
shandyoneofc <95661048+shandyoneofc@users.noreply.github.com>
2022-03-03 12:12:18 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -12,27 +12,25 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.botname = "ShandyBotz" //namabot
+global.ownername= "ShandyOneOfc" //nama owner
+global.myweb ="https://api-alphabot.herokuapp.com/"
+global.youtube = "https://youtube.com/c/ZeeoneOfc"
+global.github = "https://zeeoneofc.github.io/"
+global.ownernomer = "6285600616877" //nomor owner
+global.ownernomerr = "+6285600616877" // nmr owner
+global.thumbnail = "./image/lol.jpg" //foto bot ubah di folder image
+global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6285600616877","6285600616877","6285600616877"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© ShandyBotzོ' //sticker wm
+global.author = 'Di Buat Oleh ShandyOneOfc' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
